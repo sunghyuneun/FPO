03bcf0a1557057cf9e53caa2d1cdf860a9dd1503
sunghyuneun <gregoryeun@gmail.com>
2026-08-11 19:14:30 -0700

    CI Workflow Test 2

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -1,11 +1,18 @@
 import torch
 import gymnasium as gym
 
-print("CUDA Available:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("GPU Device:", torch.cuda.get_device_name(0))
 
-env = gym.make("Ant-v5", render_mode="rgb_array")
-obs, info = env.reset()
-print("MuJoCo Environment Loaded Successfully! Obs shape:", obs.shape)
-env.close()
+def test_installation():
+    print("CUDA Available:", torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print("GPU Device:", torch.cuda.get_device_name(0))
+
+    env = gym.make("HalfCheetah-v5")
+    obs, info = env.reset()
+    assert obs.shape == (17,), f"Gymnasium installation has a problem"
+    print("MuJoCo Environment Loaded Successfully! Obs shape:", obs.shape)
+    env.close()
+
+
+if __name__ == "__main__":
+    test_installation()
